fe23a6a62305bff8c3e64690a30aacfb8501f3b4
connerohnesorge <conneroisu@outlook.com>
2025-08-13 19:59:59 -0500

    Revert "latest"
    This reverts commit f3fab48b3db0345ae159217879cd472cb10d6796.

--- types/index.ts
+++ types/index.ts
@@ -3,27 +3,17 @@
  */
 
 // Vector types
-
-/**
- * 2D vector type
- */
 export interface Vector2 {
   readonly x: number;
   readonly y: number;
 }
 
-/**
- * 3D vector type
- */
 export interface Vector3 {
   readonly x: number;
   readonly y: number;
   readonly z: number;
 }
 
-/**
- * 4D vector type
- */
 export interface Vector4 {
   readonly x: number;
   readonly y: number;
@@ -38,25 +28,16 @@ export interface RGB {
   readonly b: number;
 }
 
-/**
- * RGBA type
- */
 export interface RGBA extends RGB {
   readonly a: number;
 }
 
-/**
- * HSL type
- */
 export interface HSL {
   readonly h: number;
   readonly s: number;
   readonly l: number;
 }
 
-/**
- * HSLA type
- */
 export interface HSLA extends HSL {
   readonly a: number;
 }
@@ -67,17 +48,11 @@ export interface Point {
   readonly y: number;
 }
 
-/**
- * Size type
- */
 export interface Size {
   readonly width: number;
   readonly height: number;
 }
 
-/**
- * Rectangle type
- */
 export interface Rectangle {
   readonly x: number;
   readonly y: number;
@@ -85,9 +60,6 @@ export interface Rectangle {
   readonly height: number;
 }
 
-/**
- * Circle type
- */
 export interface Circle {
   readonly x: number;
   readonly y: number;
@@ -111,9 +83,6 @@ export interface RandomGenerator {
   seed(value: number): void;
 }
 
-/**
- * Noise types
- */
 export interface NoiseGenerator {
   noise1D(x: number): number;
   noise2D(x: number, y: number): number;
@@ -129,9 +98,6 @@ export interface Matrix3x3 {
   ];
 }
 
-/**
- * 4x4 matrix type
- */
 export interface Matrix4x4 {
   readonly elements: readonly [
     number, number, number, number,
@@ -142,22 +108,9 @@ export interface Matrix4x4 {
 }
 
 // Utility types
-
-/**
- * Clamp a value between a min and max
- */
 export type Clamp = (value: number, min: number, max: number) => number;
-/**
- * Linear interpolation between two values
- */
 export type Lerp = (a: number, b: number, t: number) => number;
-/**
- * Map a value from one range to another
- */
 export type Map = (value: number, inMin: number, inMax: number, outMin: number, outMax: number) => number;
-/**
- * Normalize a value between a min and max
- */
 export type Normalize = (value: number, min: number, max: number) => number;
 
 // Event types for creative coding
@@ -168,9 +121,6 @@ export interface MouseEvent {
   readonly pressed: boolean;
 }
 
-/**
- * Keyboard event
- */
 export interface KeyboardEvent {
   readonly key: string;
   readonly code: string;
@@ -191,9 +141,6 @@ export type DeepReadonly<T> = {
   readonly [P in keyof T]: T[P] extends object ? DeepReadonly<T[P]> : T[P];
 };
 
-/**
- * Make a type immutable
- */
 export type Mutable<T> = {
   -readonly [P in keyof T]: T[P];
 };
